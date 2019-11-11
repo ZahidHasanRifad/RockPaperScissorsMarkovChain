246abdf9c03695c2fdafa4cb3e785b57dc73d5fe
Rifad <35569155+ZahidHasanRifad@users.noreply.github.com>
2019-11-11 22:59:03 +0600

    completed

---
+++ Rock-Paper-Scissor/src/gui/RockPaperScissorsMain.java
@@ -0,0 +1,237 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.SystemColor;
+
+import consol.Game;
+import consol.State;
+
+public class RockPaperScissorsMain {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RockPaperScissorsMain window = new RockPaperScissorsMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 * @throws IOException 
+	 */
+	public RockPaperScissorsMain() throws IOException {
+		initialize();
+	}
+	
+	private static void loadImage(ImagePanel ImagePanel, String filname)
+    {
+        try
+        {
+            BufferedImage image = ImageIO.read(new File("E:\\6th Semester\\CSE 604 Artificial Intelligence (1T+2L)\\rock paper scissors\\"+filname.toLowerCase()+".jfif"));
+            ImagePanel.setImage(image);
+        } 
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+	
+	Game game = new Game();
+	String humanResult,computerResult,tie;
+	
+	public State aiPlay(String humanState) {
+		
+		State humanChoice = State.valueOf(humanState.toUpperCase());
+		State computerChoice = game.nextMove(game.previous);
+		game.no_ofMove++;
+		
+		if(game.previous != null) {
+			game.updateTransitionModel(game.previous, humanChoice);
+			for (int i = 0; i < State.values().length; i++) {
+				for (int j = 0; j < State.values().length; j++) {
+					System.out.print(game.transitionModel[i][j] + "\t");
+				}
+				System.out.println("\n");
+			}
+			System.out.println("---------------------------------------");
+		}
+		game.previous = humanChoice;
+		
+		if(computerChoice.equals(humanChoice)) {
+			tie = "TIE";
+			game.result[2]++;
+		}
+		else if(computerChoice.losesTo(humanChoice)) {
+			humanResult = "WIN";
+			computerResult = "LOSE";
+			game.result[0]++;
+		}
+		else {
+			humanResult = "LOSE";
+			computerResult = "WIN";
+			game.result[1]++;
+		}
+		return computerChoice;
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 * @throws IOException 
+	 */
+	private void initialize() throws IOException {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(SystemColor.menu);
+		frame.setBounds(100, 100, 697, 546);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel controlPanel = new JPanel();
+		controlPanel.setBackground(SystemColor.menu);
+		controlPanel.setBounds(119, 381, 435, 100);
+		frame.getContentPane().add(controlPanel);
+		controlPanel.setLayout(null);
+		
+		JLabel aiWinCounter = new JLabel("0",SwingConstants.CENTER);
+		aiWinCounter.setBounds(267, 44, 46, 14);
+		controlPanel.add(aiWinCounter);
+		
+		JLabel humanWinCounter = new JLabel("0",SwingConstants.CENTER);
+		humanWinCounter.setBounds(130, 44, 46, 14);
+		controlPanel.add(humanWinCounter);
+		
+		JLabel aiState = new JLabel(" ",SwingConstants.CENTER);
+		aiState.setForeground(new Color(0, 0, 0));
+		aiState.setBounds(329, 39, 65, 24);
+		controlPanel.add(aiState);
+		
+		JLabel humanState = new JLabel(" ",SwingConstants.CENTER);
+		humanState.setBounds(39, 39, 65, 24);
+		controlPanel.add(humanState);
+		
+		JLabel tieLabel = new JLabel(" ",SwingConstants.CENTER);
+		tieLabel.setBounds(195, 9, 52, 24);
+		controlPanel.add(tieLabel);
+		
+		JLabel tieCounter = new JLabel("0",SwingConstants.CENTER);
+		tieCounter.setBounds(201, 44, 46, 14);
+		controlPanel.add(tieCounter);
+		//humanImageLabel = new JLabel(rockImageIcon);
+		
+		JButton rockButton = new JButton("ROCK");
+		rockButton.setForeground(Color.WHITE);
+		rockButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		rockButton.setBounds(22, 283, 89, 61);
+		frame.getContentPane().add(rockButton);
+		rockButton.setBackground(Color.GRAY);
+		rockButton.setBorder(null);
+		
+		JButton paperButton = new JButton("PAPER");
+		paperButton.setForeground(Color.WHITE);
+		paperButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		paperButton.setBounds(131, 283, 89, 61);
+		frame.getContentPane().add(paperButton);
+		paperButton.setBackground(Color.BLACK);
+		paperButton.setBorder(null);
+		
+		JButton scissorsButton = new JButton("SCISSORS");
+		scissorsButton.setForeground(Color.WHITE);
+		scissorsButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		scissorsButton.setBounds(237, 283, 89, 61);
+		frame.getContentPane().add(scissorsButton);
+		scissorsButton.setBackground(Color.RED);
+		scissorsButton.setBorder(null);
+		
+		ImagePanel humanPanel = new ImagePanel();
+		humanPanel.setBounds(57, 77, 245, 177);
+		frame.getContentPane().add(humanPanel);
+		
+		
+		JLabel lblHuman = new JLabel("Human");
+		lblHuman.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblHuman.setBounds(134, 36, 64, 14);
+		frame.getContentPane().add(lblHuman);
+		
+		ImagePanel aiPanel = new ImagePanel();
+		aiPanel.setBounds(371, 77, 245, 177);
+		frame.getContentPane().add(aiPanel);
+		
+		JLabel lblComputer = new JLabel("Computer");
+		lblComputer.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblComputer.setBounds(458, 36, 82, 14);
+		frame.getContentPane().add(lblComputer);
+		
+		game.initialize();
+		
+		scissorsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loadImage(humanPanel,scissorsButton.getText());
+				//System.out.println(scissorsButton.getText());
+				
+				String aichoice = aiPlay(scissorsButton.getText()).toString();
+				loadImage(aiPanel, aichoice);
+				aiState.setText(computerResult);
+				humanState.setText(humanResult);
+				
+				aiWinCounter.setText(Integer.toString(game.result[1]));
+				humanWinCounter.setText(Integer.toString(game.result[0]));
+				tieLabel.setText(tie);
+				tieCounter.setText(Integer.toString(game.result[2]));
+				
+			}
+		});
+		paperButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				loadImage(humanPanel,paperButton.getText());
+				
+				String aichoice = aiPlay(paperButton.getText()).toString();
+				loadImage(aiPanel, aichoice);
+				aiState.setText(computerResult);
+				humanState.setText(humanResult);
+				aiWinCounter.setText(Integer.toString(game.result[1]));
+				humanWinCounter.setText(Integer.toString(game.result[0]));
+				tieLabel.setText(tie);
+				tieCounter.setText(Integer.toString(game.result[2]));
+			}
+		});
+		rockButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loadImage(humanPanel,rockButton.getText());	
+				
+				String aichoice = aiPlay(rockButton.getText()).toString();
+				loadImage(aiPanel, aichoice);
+				aiState.setText(computerResult);
+				humanState.setText(humanResult);
+				aiWinCounter.setText(Integer.toString(game.result[1]));
+				humanWinCounter.setText(Integer.toString(game.result[0]));
+				tieLabel.setText(tie);
+				tieCounter.setText(Integer.toString(game.result[2]));
+			}
+		});
+	}
+}
